dd5b197092cf58c41eebb7095ad3a141a87c3666
Igor Shah <zombee1992@gmail.com>
2022-02-09 23:41:44 +0300

    good night

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const requestURL1 = 'https://api.icndb.com/jokes/random'
 const requestURL2 = 'https://jsonplaceholder.typicode.com/photos'
+const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
 console.log('step 1');
 let a;
+let b;
 function getData(url) {
 
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        // console.log(data);
-        console.log('----------')
-        a =  data.filter( (item,index) => index <9)
-        for(i in a){
-            console.log(i)
-            console.log(a[i].url)
-            document.querySelectorAll('.wrapper img')[i].src = a[i].url
-
-        }
-        // document.querySelector('.testimg').src = data[20].url
-        // console.log('a ===',a)
-    });
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            console.log('----------')
+            a = data.filter((item, index) => index < 9)
+            for (i in a) {
+                console.log(i)
+                console.log(a[i].url)
+                document.querySelectorAll('.wrapper img')[i].src = a[i].url
+                document.querySelectorAll('.wrapper img')[i].alt = a[i].title
+                
+            }
+        });
+    }
+function getData2(url) {
+    
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            b = data
+            // data.results[0].urls.small_s3
+            for (i in data.results) {
+                console.log('2--------2')
+                console.log(i)
+                console.log(data.results[i].urls.small_s3)
+                document.querySelectorAll('.wrapper img')[i].src = data.results[i].urls.small_s3
+                console.log(data)
+                console.log('end2---------end2')
+            }
+        });
 }
-console.log('step 2');
-// getData(requestURL1);
-console.log('step 3');
-getData(requestURL2);
-console.log('step 4');
+
+getData2(requestURL3)
+
+
 // function sendRequest(method, url, body = null) {
 //   const headers = {
 //     'Content-Type': 'application/json'
