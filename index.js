2ed309dfbbca2ff15e743fb98cd4d8341266a7ca
Igor Shah <zombee1992@gmail.com>
2022-02-10 17:36:42 +0300

    test api

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const requestURL1 = 'https://api.icndb.com/jokes/random'
 const requestURL2 = 'https://jsonplaceholder.typicode.com/photos'
 const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+//&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo
+const requestTest = 'https://api.unsplash.com/photos/?client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+// const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+//b485f3e1
+// http://www.omdbapi.com/?s=green&y=&plot=&apikey=b485f3e1 searth with name: green, year:empty, plit:none, apikey:b485f3e1
+//http://www.omdbapi.com/?t=terminator%202&y=&plot=full&apikey=b485f3e1
 console.log('step 1');
 let a;
 let b;
-function getData(url) {
-
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            // console.log(data);
-            console.log('----------')
-            a = data.filter((item, index) => index < 9)
-            for (i in a) {
-                console.log(i)
-                console.log(a[i].url)
-                document.querySelectorAll('.wrapper img')[i].src = a[i].url
-                document.querySelectorAll('.wrapper img')[i].alt = a[i].title
-                
-            }
-        });
-    }
 function getData2(url) {
     
     fetch(url)
@@ -33,33 +22,81 @@ function getData2(url) {
                 console.log(i)
                 console.log(data.results[i].urls.small_s3)
                 document.querySelectorAll('.wrapper img')[i].src = data.results[i].urls.small_s3
-                console.log(data)
+                // console.log(data)
                 console.log('end2---------end2')
             }
-        });
-}
+        })
+        .catch( i => console.log('i ====',i))
+    }
+    
+    // getData2(requestURL3)
+    function getTest(url) {
+        console.log('enter')
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            b = data
+            console.log(data)
+        })
+        // .catch( i => console.log('i ====',i))
+    }
+        
 
-getData2(requestURL3)
+    getTest(requestTest)
 
 
-// function sendRequest(method, url, body = null) {
-//   const headers = {
-//     'Content-Type': 'application/json'
-//   }
 
-//   return fetch(url, {
-//     method: method,
-//     body: JSON.stringify(body),
-//     headers: headers
-//   }).then(response => {
-//     if (response.ok) {
-//       return response.json()
-//     }
 
-//     return response.json().then(error => {
-//       const e = new Error('Что-то пошло не так')
-//       e.data = error
-//       throw e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function getData(url) {
+    
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data);
+                console.log('----------')
+                a = data.filter((item, index) => index < 9)
+                for (i in a) {
+                    console.log(i)
+                    console.log(a[i].url)
+                    // document.querySelectorAll('.wrapper img')[i].src = a[i].url
+                    // document.querySelectorAll('.wrapper img')[i].alt = a[i].title
+                }
+            })
+            .catch( i => console.log('errror',i))
+        }
+    
+    // function sendRequest(method, url, body = null) {
+        //   const headers = {
+            //     'Content-Type': 'application/json'
+            //   }
+            
+            //   return fetch(url, {
+                //     method: method,
+                //     body: JSON.stringify(body),
+                //     headers: headers
+                //   }).then(response => {
+                    //     if (response.ok) {
+                        //       return response.json()
+                        //     }
+                        
+                        //     return response.json().then(error => {
+                            //       const e = new Error('Что-то пошло не так')
+                            //       e.data = error
+                            //       throw e
 //     })
 //   })
 // }
@@ -69,9 +106,9 @@ getData2(requestURL3)
 //   .catch(err => console.log(err))
 
 // const body = {
-//   name: 'Vladilen',
-//   age: 26
-// }
+    //   name: 'Vladilen',
+    //   age: 26
+    // }
 // console.log(body)
 // sendRequest('POST', requestURL, body)
 //   .then(data => console.log(data))
