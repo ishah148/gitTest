988c0fcac2c870e94ab17202b43ef4e21ff4e935
Igor Shah <zombee1992@gmail.com>
2022-02-13 17:47:53 +0300

    adding content decription

--- index.js
+++ index.js
@@ -1,78 +1,51 @@
 const requestURL1 = 'https://api.icndb.com/jokes/random'
 const requestURL2 = 'https://jsonplaceholder.typicode.com/photos'
 const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
-//&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo
 const requestTest = 'https://api.unsplash.com/photos/?client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
-// const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
-//b485f3e1
-// http://www.omdbapi.com/?s=green&y=&plot=&apikey=b485f3e1 searth with name: green, year:empty, plit:none, apikey:b485f3e1
-//http://www.omdbapi.com/?t=terminator%202&y=&plot=full&apikey=b485f3e1
 
-// =============== api tmdb ===============
-// =============== some examples===============
-//api tmdb
-// f5978d3a7a7427ea73c7d60edf76ed30
-//https://api.themoviedb.org/3/movie/550?api_key=f5978d3a7a7427ea73c7d60edf76ed30
-//https://api.themoviedb.org/3/search/movie?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&query=1984&page=1&include_adult=false
-//search 1984 by name
-// Path to photo is link + ID of film
-// =============== popular ===============
-// https://api.themoviedb.org/3/movie/popular?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&page=1
-// =============== img link ===============
-// https://www.themoviedb.org/t/p/w220_and_h330_face/
-// ===============    end      ===============
-
-//cd3f65717cbbf12fee2f4b6eac20629e apikey kinopoisk
-
-// temp = '<div class="table"><img src="" alt="img" class="testimg"><div class="description-wrapper"><p class="title"></p><p class="year"></p><svg class="rating-svg"><use xlink:href="assets/svg/rating.svg#rating"></use></svg><p class="rating"></p></div></div>'
-temp = (`        <div class="table">
+filmContent = (`        <div class="table">
 <img src="" alt="img" class="testimg">
 <div class="description-wrapper">
     <p class="title"></p>
-    <p class="year"></p>
     <p class="rating"></p>
 </div>
 </div>`)
+
 let numberOfPage = 1;
-const requestUrlPopular =     'https://api.themoviedb.org/3/movie/popular?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&page=1'
 const requestUrlPopularPage = `https://api.themoviedb.org/3/movie/popular?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&page=`
 const requestSearch = (searchByName) =>'https://api.themoviedb.org/3/search/movie?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&query='+searchByName+'&page=1&include_adult=false';
-// function requestSearch
+
+const content =  document.querySelector('.table')
 const imgLink = 'https://www.themoviedb.org/t/p/w220_and_h330_face'
 let a;
 let b;
-// getData(requestUrlPopular)
-getFilmResult(requestUrlPopular)
+
 // getFilmResult(requestUrlPopularPage)
 
+
 function getFilmResult(url) {
-    // getFilm(requestUrlPopular)
-    // getFilm(requestUrlPopular2)
-    // let dataFilm
+    // pull data from API
     async function getFilm(url){
         try {
             console.log('get1')
             const response = await fetch(url)
             const data = await response.json()
             // return Promise.resolve(data)
-            console.log(data)
+            // console.log(data)
             return data
         } catch(err){
         console.log(err)
     }
     }
-
+    // add html wrapper
     getFilm(url).then( data => {
         console.log('get2')
-
-        // console.log(data.results.length)
         for(i in data.results){
-            // if(i > data.results.length - 1) return
-            document.querySelector('.wrapper').insertAdjacentHTML('afterbegin', temp)
+            document.querySelector('.wrapper').insertAdjacentHTML('afterbegin', filmContent)
 
         }
     })
-
+    // add content wrapper
     getFilm(url).then( data => {
         console.log('get3')
         
@@ -84,7 +57,6 @@ function getFilmResult(url) {
             document.querySelectorAll('.table .rating')[i].innerHTML = data.results[i].vote_average
         }
     })
-// getFilm.then( data => console.log(data))
 
 }
 
@@ -107,11 +79,7 @@ function minusPage(){
     document.querySelector('nav .current p').innerHTML = numberOfPage
     getFilmResult(`${requestUrlPopularPage}${numberOfPage}`)
 }
-// function search(){
     
-// }
-
-
 let form = document.getElementById('search-form');
 form.onsubmit = function() {
     let value = form.text.value;
@@ -122,3 +90,11 @@ form.onsubmit = function() {
     // complete(value);
     return false;
   };
+//   content.addEventListener('click', showAdditionalData)
+  content.addEventListener('click', () =>{
+      console.log('click')
+  })
+  
+//   function showAdditionalData(){
+//    content.style.display = 'none'
+//   }
---
+++ temp.js
@@ -0,0 +1,37 @@
+
+//&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo
+// const requestUrlPopular =     'https://api.themoviedb.org/3/movie/popular?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&page=1'
+
+// const requestURL3 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+//b485f3e1
+// http://www.omdbapi.com/?s=green&y=&plot=&apikey=b485f3e1 searth with name: green, year:empty, plit:none, apikey:b485f3e1
+//http://www.omdbapi.com/?t=terminator%202&y=&plot=full&apikey=b485f3e1
+
+// =============== api tmdb ===============
+// =============== some examples===============
+//api tmdb
+// f5978d3a7a7427ea73c7d60edf76ed30
+//https://api.themoviedb.org/3/movie/550?api_key=f5978d3a7a7427ea73c7d60edf76ed30
+//https://api.themoviedb.org/3/search/movie?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&query=1984&page=1&include_adult=false
+//search 1984 by name
+// Path to photo is link + ID of film
+// =============== popular ===============
+// https://api.themoviedb.org/3/movie/popular?api_key=f5978d3a7a7427ea73c7d60edf76ed30&language=en-US&page=1
+// =============== img link ===============
+// https://www.themoviedb.org/t/p/w220_and_h330_face/
+// ===============    end      ===============
+
+//cd3f65717cbbf12fee2f4b6eac20629e apikey kinopoisk
+
+
+// temp = '<div class="table"><img src="" alt="img" class="testimg"><div class="description-wrapper"><p class="title"></p><p class="year"></p><svg class="rating-svg"><use xlink:href="assets/svg/rating.svg#rating"></use></svg><p class="rating"></p></div></div>'
+
+//old content
+filmContent = (`        <div class="table">
+<img src="" alt="img" class="testimg">
+<div class="description-wrapper">
+    <p class="title"></p>
+    <p class="year"></p>
+    <p class="rating"></p>
+</div>
+</div>`)
