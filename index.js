c6627be0ad5c675196e5451979e887fefe4baa19
Igor Shah <zombee1992@gmail.com>
2022-03-11 11:25:49 +0300

    reformat: del files

--- index.js
+++
@@ -1,8 +0,0 @@
-const description = document.querySelectorAll('.project')
-// description.addEventListener('click',()=>{
-//     document.querySelectorAll('.description-project')[0].classList.toggle('push')
-// }
-// )
-description.forEach( (item,index) => item.addEventListener('click',()=>{
-    item.firstElementChild.classList.toggle('push')
-}))
