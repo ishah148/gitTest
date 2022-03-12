146b1e21950fe620d9b6d1ee47fc2f28c7da4e35
Igor Shah <zombee1992@gmail.com>
2022-03-12 16:27:41 +0300

    explode

--- stage1/file.js
+++ stage1/file.js
@@ -1,24 +1,24 @@
 let a = "1 2 3 4 5"
 
-a =  "1 2 -3 4 5"
-function highAndLow(numbers){
+a = "1 2 -3 4 5"
+function highAndLow(numbers) {
     let temp = numbers.split(' ')
         .map(item => +item)
-        .sort((a,b) =>b-a)
+        .sort((a, b) => b - a)
 
     return `${temp[0]} ${temp[temp.length - 1]}`;
-  }
+}
 
 // highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4')
 a = "This website is for losers LOL!"
 function disemvowel(str) {
-    listSogl = ["a", "e", "i", "o", "u","A","E","I","O","U"]
-    let temp = str.split('').map(item => listSogl.includes(item)?'':item)
+    listSogl = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    let temp = str.split('').map(item => listSogl.includes(item) ? '' : item)
     return temp.join('')
-  }
-console.log(disemvowel(a))
+}
+// console.log(disemvowel(a))
 
-function isIsogram(str){
+function isIsogram(str) {
     temp = new Set(str.toUpperCase().split(''))
     return temp.size === str.split('').length
 }
@@ -26,3 +26,13 @@ function isIsogram(str){
 // console.log(isIsogram("Dermatoglyphics"))
 // console.log(isIsogram("aba"))
 
+function explode(s) {
+    return s.split('')
+        .map(item => item.repeat(+item))
+        .join('')
+        + ''
+}
+
+// console.log(explode("312"))
+// console.log(explode("333122"))
+// explode('1488')
