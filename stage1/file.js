b8fd2515036677f60072086824d0cbec79a5cb3c
Igor Shah <zombee1992@gmail.com>
2022-03-12 14:04:55 +0300

    Highest and Lowest

--- stage1/file.js
+++ stage1/file.js
@@ -1,1 +1,12 @@
-console.log(123)
+let a = "1 2 3 4 5"
+
+a =  "1 2 -3 4 5"
+function highAndLow(numbers){
+    let temp = numbers.split(' ')
+        .map(item => +item)
+        .sort((a,b) =>b-a)
+
+    return `${temp[0]} ${temp[temp.length - 1]}`;
+  }
+
+highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4')
