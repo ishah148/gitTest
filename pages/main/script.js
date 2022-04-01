96951e61cbe05b1d76dd334994fef1bb0f64fc6e
Igor Shah <zombee1992@gmail.com>
2022-04-02 00:08:11 +0300

    burger menu ...

--- pages/main/script.js
+++ pages/main/script.js
@@ -2,3 +2,12 @@ console.log(window.innerWidth)
 window.addEventListener('resize',()=>{
     console.log(this.innerWidth)
 })
+const elems = {
+    nav : document.querySelector("nav"),
+    hamburger : document.querySelector(".hamburger"),
+}
+elems.hamburger.addEventListener('click',function (){
+    this.classList.toggle('active')
+    console.log(this)
+    elems.nav.classList.toggle('active')
+})
