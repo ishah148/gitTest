3f6b705d1fa077bc4623997cbbb7b89e293e0713
Igor Shah <zombee1992@gmail.com>
2022-04-04 17:27:08 +0300

    123

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,13 +1,24 @@
-console.log(window.innerWidth)
-window.addEventListener('resize',()=>{
-    console.log(this.innerWidth)
-})
 const elems = {
     nav : document.querySelector("nav"),
     hamburger : document.querySelector(".hamburger"),
 }
+
+console.log(window.innerWidth)
+
+window.addEventListener('resize',()=>{
+    console.log(this.innerWidth)
+})
+
 elems.hamburger.addEventListener('click',function (){
     this.classList.toggle('active')
-    console.log(this)
     elems.nav.classList.toggle('active')
+})
+
+document.querySelectorAll('nav p').forEach(block =>{
+    block.addEventListener('click',function(){
+    if(window.innerWidth < 560){
+        elems.nav.classList.toggle('active')
+        elems.hamburger.classList.toggle('active')
+    }
+    })
 })
