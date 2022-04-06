652eaea2e0ae77bcbaa60badee5e6df7611beca5
Igor Shah <zombee1992@gmail.com>
2022-04-06 22:42:38 +0300

    add header.js

---
+++ pages/pets/header.js
@@ -0,0 +1,17 @@
+window.onload = updateHeader();
+
+window.addEventListener('scroll', function() {
+    if(pageYOffset){
+        document.querySelector('header').classList.add('header__fixed');
+    }
+    
+    if(!pageYOffset){
+        document.querySelector('header').classList.remove('header__fixed');
+    }
+});
+
+window.addEventListener('resize',updateHeader);
+
+function updateHeader(){
+    document.querySelector('header').style.width = document.querySelector('body').offsetWidth + 'px'
+}
