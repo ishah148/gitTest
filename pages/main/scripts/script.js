5fa355f8ffc7bbe61a2541a877425af35d6b8e91
Igor Shah <zombee1992@gmail.com>
2022-04-07 19:04:43 +0300

    fix pets list

--- pages/main/scripts/script.js
+++ pages/main/scripts/script.js
@@ -1,30 +1,45 @@
 // import {* as x} from 'pages/pets/pets-list.js'
-import {pets} from './pets-list.js';
-
-console.log(...pets.map(i => console.log(i.name)))
-
-
 const elems = {
-    nav : document.querySelector("nav"),
-    hamburger : document.querySelector(".hamburger"),
+    nav: document.querySelector("nav"),
+    hamburger: document.querySelector(".hamburger"),
+    content:document.querySelector('.pets .content-wrapper')
 }
+import { pets } from './pets-list.js';
+console.log(pets)
+console.log(pets.map(i => console.log(i.img)))
+pets.forEach(pet => elems.content.insertAdjacentHTML('beforeend',getCard(pet)))
+elems.content.insertAdjacentHTML('beforeend',getCard(pets[1]))
+
+function getCard(obj) {
+    return `
+    <div class="card">
+        <div class="image"><img src="${obj.img}" alt=""></div>
+        <p class="pets-card-title">${obj.name}</p>
+        <button class="button-primary">Learn more</button>
+    </div>
+ `
+}
+
+
+
+
 
 console.log(window.innerWidth)
 
-window.addEventListener('resize',()=>{
+window.addEventListener('resize', () => {
     // console.log(this.innerWidth)
 })
 
-elems.hamburger.addEventListener('click',function (){
+elems.hamburger.addEventListener('click', function () {
     this.classList.toggle('active')
     elems.nav.classList.toggle('active')
 })
 
-document.querySelectorAll('nav p').forEach(block =>{
-    block.addEventListener('click',function(){
-    if(window.innerWidth < 660){
-        elems.nav.classList.toggle('active')
-        elems.hamburger.classList.toggle('active')
-    }
+document.querySelectorAll('nav p').forEach(block => {
+    block.addEventListener('click', function () {
+        if (window.innerWidth < 660) {
+            elems.nav.classList.toggle('active')
+            elems.hamburger.classList.toggle('active')
+        }
     })
 })
