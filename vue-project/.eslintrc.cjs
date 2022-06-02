87695b893a1e5862e771413ebb99b159739e9684
Igor Shah <zombee1992@gmail.com>
2022-06-02 22:43:49 +0300

    123 2

---
+++ vue-project/.eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-tabs": ["error", { allowIndentationTabs: true }],
+    // "prettier/prettier": "error",
+    // "useTabs": 2,
+    // tabWidth: 4,
+  },
+};
---
+++ vue-project/src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+// import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+    <div class="todo-wrapper">
+        <header>
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" />
+            <HelloWorld msg="You did it!" />
+            <nav>
+                <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+                <h1>TODO list</h1>
+            </nav>
+        </header>
+        <div class="add">
+            <input type="text" placeholder="input here!" class="add__input" />
+            <button class="add__button">add</button>
+        </div>
+    </div>
+    <!-- <RouterView /> -->
+</template>
+     
+<style lang="scss">
+.todo-wrapper {
+    width: 100%;
+    color: black;
+    & * {
+        box-sizing: border-box;
+    }
+}
+
+header {
+    padding: 20px;
+    max-width: 1400px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    img {
+        width: 50px;
+        height: 50px;
+    }
+
+    .wrapper {
+        width: 500px;
+    }
+}
+.add {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    justify-content: center;
+    gap: 20px;
+    .add__input {
+        width: 30%;
+        height: 30px;
+        padding: 0;
+        margin: 0;
+    }
+    .add__button {
+        width: 100px;
+        height: 30px;
+        background: transparent;
+    }
+}
+</style>
