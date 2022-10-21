6cd789e7cee09f164086a786cffed8bcba37733b
Igor Shah <zombee1992@gmail.com>
2022-10-21 13:58:25 +0300

    123

--- project2/src/views/HomeView.vue
+++ project2/src/views/HomeView.vue
@@ -3,16 +3,19 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <SearchBar />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from '../components/SearchBar.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    SearchBar
   }
 })
 </script>
